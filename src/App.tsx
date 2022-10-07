c785ba87eda21f85fcbc65b1af7f717b14b23485
alanbacont <almabarocioco@ittepic.edu.mx>
2022-10-06 20:10:44 -0600

    commit 2 - 06 oct 22
    Fin de nota 4.6

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
-import { ObjetosLiterales } from './typescript/ObjetosLiterales';
+//import { ObjetosLiterales } from './typescript/ObjetosLiterales';
 //import { TiposBasicos } from './typescript/TiposBasicos';
+import { Contador } from './components/Contador';
 const App = () => {
   return (
     <div>
       <h1>Introducción a Type Script with React</h1>
       <hr/>
       {/*<TiposBasicos> </TiposBasicos>*/}
-      <ObjetosLiterales/>
+      {/*<ObjetosLiterales/>*/}
+      {<Contador/>}
     </div>
   )
 }
